1a0d5d197f6ea8c7f502af57fa60b66473d226b6
Your <samkiragu05@gmail.com>
2023-03-21 02:20:41 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A program that prints a line without using the standard library
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+write("_putchar\n");
+return (0);
+}
