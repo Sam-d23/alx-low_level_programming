caf30e872b553093895b4d9808eb9d386b60b07b
Your <samkiragu05@gmail.com>
2023-03-21 19:37:40 -0700

    Task zero

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,2 @@
-#include<unistd.h>
-/**
- * _putchar - writes the character c to stdout
- * @c - the character to print
- * Return: On Success 1.
- * On error, -1 is returned and errno set appropriately.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+int  _putchar(char c);
 
