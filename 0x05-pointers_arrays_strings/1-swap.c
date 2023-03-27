83c4f9e2e0d8eb21558ee121e587f05428839c0b
Your <samkiragu05@gmail.com>
2023-03-27 10:34:43 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * void swap_int - changes values of integers
+ * @a: int to swap
+ * @b: int to swap
+ */
+void swap_int(int *a, int *b);
+{
+int x;
+x = *a;
+*a = *b;
+*b = m;
+}
