17eb087c898146bf77e8a9389a6b4d52ac188e8e
Your <samkiragu05@gmail.com>
2023-03-27 11:45:31 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts - prints a string, followed by a new line, to stdout.
+ *_ puts - prints a string, followed by a new line, to stdout.
  * @str: string to be printed
  */
 void _puts(char *str)
