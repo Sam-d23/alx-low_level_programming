d3957075123a110db142942cb99b8b6100a48788
Your <samkiragu05@gmail.com>
2023-03-27 16:37:33 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copies a string
+ * @dest: Destination value
+ * @src: source value
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int j;
+for (j = 0; str[j] != '\0'; j++)
+{
+dest[j] = src[j];
+}
+dest[j++] = '\0';
+return (dest);
+}
