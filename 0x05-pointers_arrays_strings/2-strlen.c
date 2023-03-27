51462a763513285594fe91e67bfcec348a231202
Your <samkiragu05@gmail.com>
2023-03-27 11:00:38 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *S)
+{
+int the_length = 0;
+while (*s != '\0')
+{
+the_length++;
+s++;
+}
+return (the_length);
+}
