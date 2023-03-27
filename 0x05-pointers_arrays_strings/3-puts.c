c2f61a112285161a68b229c6c155c529dddca458
Your <samkiragu05@gmail.com>
2023-03-27 11:49:02 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_ puts - prints a string, followed by a new line, to stdout.
+ * _puts - prints a string, followed by a new line, to stdout.
  * @str: string to be printed
  */
 void _puts(char *str)
