172963099c9bbad25f31a64481abc3798825db05
Your <samkiragu05@gmail.com>
2023-03-27 17:04:10 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer that converts
+ * Return: an integer
+ */
+int _atoi(char *s)
+{
+int a = 0;
+unsigned int un = 0; 
+int max  = 1;
+int sec = 0;
+while (s[a])
+{
+if (s[a] == 45)
+{
+max *= -1;
+}
+while (s[a] >= 48 && s[a] <=57)
+{
+sec = 1;
+un = (un *10) + (s[a] - '0');
+a++;
+}
+if (sec == 1)
+{
+break;
+}
+a++;
+}
+un *= max;
+return (un);
+}
