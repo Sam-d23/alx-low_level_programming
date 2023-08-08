9c9a352eb4b4b633f61400f04d1e5c6d5ce17820
Sam-d23 <samkiragu05@gmail.com>
2023-08-08 05:29:52 -0700

    Task 005

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,12 +13,13 @@ int i, j, k, len;
 char *str;
 if (ac == 0 || av == NULL)
 	return (NULL);
+len = 0;
 for (i = 0; i < ac; i++)
 {
 for (j = 0; av[i][j] != '\0'; j++)
 len++;
 }
-str = malloc(sizeof(char) * (len + 1));
+str = malloc(sizeof(char) * (len));
 if (str == NULL)
 	return (NULL);
 k = 0;
