5dfaf436313504e275e01d76dec9c1c935bf80c0
Sam-d23 <samkiragu05@gmail.com>
2023-08-08 05:33:26 -0700

    correction

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,13 +13,13 @@ int i, j, k, len;
 char *str;
 if (ac == 0 || av == NULL)
 	return (NULL);
-len = 0;
 for (i = 0; i < ac; i++)
 {
 for (j = 0; av[i][j] != '\0'; j++)
+	len++;
 len++;
 }
-str = malloc(sizeof(char) * (len));
+str = malloc(sizeof(char) * (len + 1));
 if (str == NULL)
 	return (NULL);
 k = 0;
