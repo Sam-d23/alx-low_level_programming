a4ce0146bd133cb16d4a09452a0947bc287ac254
Sam-d23 <samkiragu05@gmail.com>
2023-08-08 05:36:42 -0700

    new

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -16,10 +16,9 @@ if (ac == 0 || av == NULL)
 for (i = 0; i < ac; i++)
 {
 for (j = 0; av[i][j] != '\0'; j++)
-	len++;
 len++;
 }
-str = malloc(sizeof(char) * (len + 1));
+str = malloc(sizeof(char) * (len));
 if (str == NULL)
 	return (NULL);
 k = 0;
