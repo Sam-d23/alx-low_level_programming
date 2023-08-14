45267bbd2a60db931891f5234d00a95efb613df0
Sam-d23 <samkiragu05@gmail.com>
2023-08-14 09:47:57 -0700

    TASK ZERO

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - a dog's basic info
- * @name: first member
- * @age: second entry
+ * struct dog - dog's basic info
+ * @name: name to initialize
+ * @age: age to initialize
  * @owner: owner to initialize
  */
 struct dog
@@ -15,11 +15,10 @@ char *owner;
 /**
  * dog_t - typedef for struct dog
  */
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 #endif
