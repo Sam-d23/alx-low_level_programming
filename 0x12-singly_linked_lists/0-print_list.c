12d41ec777ddaf0a9b4ca87f0282a18a3f43c2b6
Sam-d23 <samkiragu05@gmail.com>
2023-08-24 22:55:44 +0300

    Updated task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,9 +13,9 @@ size_t size = 0;
 while (h)
 {
 if (h->str == NULL)
-	printf("|0| (nil)\n");
+	printf("[0] (nil)\n");
 else
-	printf("|%u| %s\n", h->len, h->str);
+	printf("[%d] %s\n", h->len, h->str);
 size++;
 h = h->next;
 }
