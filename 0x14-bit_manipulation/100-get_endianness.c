804e7a558bdaf176d5061adfbe21f8c1c08c0eca
Sam-d23 <samkiragu05@gmail.com>
2023-09-09 04:32:01 -0700

    task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * get_endianness -  checks the endianness
+ * Return: 0 (big endian), 1 (little endian)
+ */
+int get_endianness(void)
+{
+unsigned int j = 1;
+char *c = (char *) &j;
+return (*c);
+}
