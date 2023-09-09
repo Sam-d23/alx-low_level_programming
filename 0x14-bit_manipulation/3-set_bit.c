36f06a7b2651af0f3760ea8d4508a5e8e1ed1c97
Sam-d23 <samkiragu05@gmail.com>
2023-09-09 03:32:48 -0700

    correction

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * set_bit - sets the bit value to 1 at an index
- * @*n: pointer to the number being changed
+ * @n: pointer to the number being changed
  * @index: bit index
  * Return: 1 (Success), 0 (failure)
  */
