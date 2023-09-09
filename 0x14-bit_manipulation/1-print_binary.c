923c6f41e8a8de2ec4cfd7bc34265fa39752ae03
Sam-d23 <samkiragu05@gmail.com>
2023-09-09 02:40:01 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary equivalent for number
+ * @n: number to be represented
+ */
+void print_binary(unsigned long int n)
+{
+int j, rec = 0;
+unsigned long int number;
+for (j = 63; j >= 0; j--)
+{
+number = n >> j;
+if (number & 1)
+{
+_putchar('1');
+rec++;
+}
+else if (rec)
+	_putchar('0');
+}
+if (!rec)
+	_putchar('0');
+}
