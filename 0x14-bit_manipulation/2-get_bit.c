b28712f4351d8212d973a682684649db2e7454f7
Sam-d23 <samkiragu05@gmail.com>
2023-09-28 14:39:50 -0700

    Task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: number to be assessed
+ * @index: index of bit 
+ * Return: value of index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+int value;
+if (index > 63)
+	return (-1);
+value = (n >> index) & 1;
+return (value);
+}
