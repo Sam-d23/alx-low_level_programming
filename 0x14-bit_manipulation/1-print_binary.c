66b1f1820a60f4160801c5641aa16ec25d9b5b33
Sam-d23 <samkiragu05@gmail.com>
2023-09-28 10:46:04 -0700

    Task One

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,41 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _pow - calculates power
- * @base: exponent's base
- * @power: exponent's power
- * Return: value of result
- */
-unsigned long int _pow(unsigned int base, unsigned int power)
-{
-unsigned long int number;
-unsigned int i;
-number = 1;
-for (i = 1; i <= power; i++)
-	number *= base;
-return (number);
-}
-/* print_binary - prints binary representation
- * @n: decimal number to be represented
+ * print_binary - prints binary representative
+ * @n: number to be represented
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-unsigned long int divisor, checker;
-char state;
-state = 0;
-divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
-while (divisor != 0)
+unsigned int checker = 32768;
+while (checker > 0)
 {
-checker = n & divisor;
-if (checker == divisor)
-{
-state = 1;
-_putchar('1');
-}
-else if (state == 1 || divisor == 1)
-{
-_putchar('0');
-}
-divisor >>= 1;
+if ((n & checker) == 0)
+	_putchar('0');
+else
+	_putchar('1');
+checker = checker >> 1;
 }
 }
