77537732bfd972aa19ddc8a241a9a0668b05f09f
Sam-d23 <samkiragu05@gmail.com>
2023-09-28 13:29:16 -0700

    Trial.1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,12 @@
  */
 void print_binary(unsigned long int n)
 {
+int j;
+unsigned long int bit;
+bit = n >> j;
 unsigned int checker = 32768;
+for (j = 63; j >= 0; j--)
+{
 while (checker > 0)
 {
 if ((n & checker) == 0)
@@ -15,5 +20,6 @@ if ((n & checker) == 0)
 else
 	_putchar('1');
 checker = checker >> 1;
+}
 }
 }
