c2b5c581897870d9214b7c58e73cc8f16c786f65
Sam-d23 <samkiragu05@gmail.com>
2023-10-06 13:24:23 -0700

    4 task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -16,11 +16,11 @@ void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 /**
- * check_elf - checks whether a file is an e-file
+ * check_elf_plus_print_magic - checks whether a file is an e-file
  * @e_ident: pointer to file having ELF magic numbers
  * if not ELF file, exit code 98
  */
-void check_elf(unsigned char *e_ident)
+void check_elf_plus_print_magic(unsigned char *e_ident)
 {
 int i;
 for (i = 0; i < 4; i++)
@@ -32,6 +32,15 @@ dprintf(STDERR_FILENO, "Error: This is not an elf file\n");
 exit(98);
 }
 }
+int j;
+printf("   Magic:  ");
+for (j = 0; j < EI_NIDENT; j++)
+{
+printf("%02x", e_ident[j]);
+if (j == EI_NIDENT - 1)
+        printf("\n");
+else
+        printf(" ");
 }
 /**
  * print_data - prints ELF header data
@@ -56,28 +65,10 @@ printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 }
 }
 /**
- * print_magic - prints magic numbers
- * @@e_ident: pointer to magic numbers array
- * Magic numbers separated by spaces
- */
-void print_magic(unsigned char *e_ident)
-{
-int i;
-printf("   Magic:  ");
-for (i = 0; i < EI_NIDENT; i++)
-{
-printf("%02x", e_ident[i]);
-if (i == EI_NIDENT - 1)
-	printf("\n");
-else
-	printf(" ");
-}
-}
-/**
- * print_class - prints
+ * print_class_plus_print_version - prints class and version
  * @e_ident: pointer
  */
-void print_class(unsigned char *e_ident)
+void print_class_plus_print_version(unsigned char *e_ident)
 {
 printf("   Class:                     ");
 switch (e_ident[EI_CLASS])
@@ -94,22 +85,15 @@ case ELFCLASS64:
 default:
 	printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 }
-}
-/**
- * print_version - prints ELF header version
- * @e_ident: pointer to array of ELF version
- */
-void print_version(unsigned char *e_ident)
-{
 printf("   Version:      %d", e_ident[EI_VERSION]);
 switch (e_ident[EI_VERSION])
 {
 case EV_CURRENT:
-	printf(" (current)\n");
+        printf(" (current)\n");
 break;
 default:
-	printf("\n");
-	break;
+        printf("\n");
+        break;
 }
 }
 /**
@@ -156,19 +140,11 @@ default:
 }
 }
 /**
- * print_abi - prints ELF header ABI Version
- * @e_ident: pointer to array of ELF ABI version
- */
-void print_abi(unsigned char *e_ident)
-{
-printf("  ABI Version:     %d\n", e_ident[EI_ABIVERSION]);
-}
-/**
- * print_type - prints type of ELF header
+ * print_type_plus_print_abi - prints type of ELF header
  * @e_type: ELF type
  * @e_ident: pointer to array of ELF class
  */
-void print_type(unsigned int e_type, unsigned char *e_ident)
+void print_type_plus_print_abi(unsigned int e_type, unsigned char *e_ident)
 {
 if (e_ident[EI_DATA] == ELFDATA2MSB)
 	e_type >>= 8;
@@ -193,13 +169,15 @@ case ET_CORE:
 default:
 	printf("<unknown: %x>\n", e_type);
 }
+printf("  ABI Version:     %d\n", e_ident[EI_ABIVERSION]);
 }
 /**
  * print_entry - prints ELF header entry point
  * @e_entry: ELF entry point address
  * @e_ident: pointer to array of ELF class
+ * @elf: ELF file descriptor
  */
-void print_entry(unsigned long int e_entry, unsigned char *e_ident)
+void print_entry(unsigned long int e_entry, unsigned char *e_ident, int elf)
 {
 printf("  Entry point address:               ");
 if (e_ident[EI_DATA] == ELFDATA2MSB)
@@ -214,14 +192,6 @@ else
 {
 printf("%#lx\n", e_entry);
 }
-}
-/**
- * close_elf - function that closes an ELF file.
- * @elf: ELF file descriptor.
- * Description: If the file doesn't close - exit code 98.
- */
-void close_elf(int elf)
-{
 if (close(elf) == -1)
 {
 dprintf(STDERR_FILENO,
