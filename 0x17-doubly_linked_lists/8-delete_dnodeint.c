db43769eb944de90af2b270703767863c547b812
Sam-d23 <samkiragu05@gmail.com>
2023-11-10 02:53:38 -0800

    task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes node at index a
+ * @head: new node's head
+ * @index: new node's index
+ * Return: 1 (Success), else -1
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+        dlistint_t *h_1;
+        dlistint_t *h_1;
+        unsigned int j;
+
+        h_1 = *head;
+
+        if (h_1 != NULL)
+                while (h_1->prev != NULL)
+                        h_1 = h_1->prev;
+
+        j = 0;
+
+        while (h_1 != NULL)
+        {
+                if (j == index)
+		{
+			if (j == 0)
+			{
+				*head = h_1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h_2->next = h_2->next;
+
+				if (h_1->next != NULL)
+					h_1->next->prev = h_2;
+			}
+
+			free(h_1);
+			return (1);
+		}
+		h_2 = h_1;
+		h_1 = h_1->next;
+		j++;
+        }
+
+	return (-1);
+}
+
