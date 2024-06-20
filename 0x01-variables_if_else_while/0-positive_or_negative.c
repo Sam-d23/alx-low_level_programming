32c1e9a0877c175ff771109fbc0bc0f1a696706e
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-20 05:39:53 +0300

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* 
+/*
  * main - Entry point of the program.
  *
  * Description: This program assigns a random number to the variable n
