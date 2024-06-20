05cdbc3af848cb34ce1fdfc0e8ace2469893dad9
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-20 06:09:31 +0300

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,31 +13,31 @@
  */
 int main(void)
 {
-    int n;
-    int last_digit;
+int n;
+int last_digit;
 
-    /* Seed the random number generator using the current time */
-    srand(time(0));
-    /* Assign a random number to n, shifted to include negative numbers */
-    n = rand() - RAND_MAX / 2;
-    /* Calculate the last digit of n */
-    last_digit = n % 10;
+/* Seed the random number generator using the current time */
+srand(time(0));
+/* Assign a random number to n, shifted to include negative numbers */
+n = rand() - RAND_MAX / 2;
+/* Calculate the last digit of n */
+last_digit = n % 10;
 
-    /* Print the last digit and the corresponding message */
-    printf("Last digit of %d is %d and is ", n, last_digit);
-    if (last_digit > 5)
-    {
-        printf("greater than 5\n");
-    }
-    else if (last_digit == 0)
-    {
-        printf("0\n");
-    }
-    else
-    {
-        printf("less than 6 and not 0\n");
-    }
+/* Print the last digit and the corresponding message */
+printf("Last digit of %d is %d and is ", n, last_digit);
+if (last_digit > 5)
+{
+	printf("greater than 5\n");
+}
+else if (last_digit == 0)
+{
+	printf("0\n");
+}
+else
+{
+	printf("less than 6 and not 0\n");
+}
 
-    /* Return 0 to indicate successful execution */
-    return (0);
+/* Return 0 to indicate successful execution */
+return (0);
 }
