07d62486763745aaff6553df0c278aaa11a7a861
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-21 06:57:22 +0300

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,22 +10,22 @@
  */
 int main(void)
 {
-    char ch;
+	char ch;
 
-    /* Print lowercase alphabet */
-    for (ch = 97; ch <= 122; ch++)
-    {
-        putchar(ch);
-    }
+	/* Print lowercase alphabet */
+	for (ch = 97; ch <= 122; ch++)
+	{
+        	putchar(ch);
+	}
 
-    /* Print uppercase alphabet */
-    for (ch = 65; ch <= 90; ch++)
-    {
-        putchar(ch);
-    }
+	/* Print uppercase alphabet */
+   	for (ch = 65; ch <= 90; ch++)
+	{
+        	putchar(ch);
+	}
 
-    /* Print new line */
-    putchar('\n');
+	/* Print new line */
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
