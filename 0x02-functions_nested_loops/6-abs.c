e3a2e849b4833a2038b0f8cbc8f9b982e8e5e9ca
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-27 10:27:44 +0300

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * Return: absolute value.
+ */
+int _abs(int)
+{
+	int a;
+
+	return (abs(a));
+}
