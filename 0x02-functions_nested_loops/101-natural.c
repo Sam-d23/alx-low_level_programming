388d3b790ffe7bfa8e98ded11abaaf9f6d6111dc
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-27 17:02:21 +0300

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-/** main - prints the sum of all multiples of 3 or 5.
+/** 
+ * main - prints the sum of all multiples of 3 or 5.
  *
  * Return: 0 (Always)
  */
 int main(void)
 {
 	int i, sum = 0;
+
 	for (i = 1; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
