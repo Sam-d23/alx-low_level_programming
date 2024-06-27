bf9783430252010cbbf174e5b107a1197c8b803a
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-27 17:23:12 +0300

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Sum of even-valued Fionacci sequence terms
+ * up to 4,000,000 is printed.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum_of_evens = 0;
+	int x;
+	int y;
+	int sum = 1;
+
+	x = 1;
+	y = 1;
+
+	while (y < 4000000)
+	{
+		sum = x + y;
+		x = y;
+		y = sum;
+		if ((sum <= 4000000) && (sum % 2 == 0))
+			sum_of_evens += sum;
+	}
+	printf("%d\n", sum_of_evens);
+
+	return (0);
+}
